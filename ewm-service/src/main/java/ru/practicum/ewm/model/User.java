7602eac26099bc76cb9eca42138ca02fd55eec89
JJJ0103 <jjj1.03@yandex.ru>
2025-03-06 00:50:27 +0300

    //

---
+++ ewm-service/src/main/java/ru/practicum/ewm/model/User.java
@@ -0,0 +1,21 @@
+package ru.practicum.ewm.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+}
